334da639e30e851ed4920180cd50d074a5d37b83
PoLoMoTo <barnwell9@gmail.com>
2020-02-21 23:49:05 -0500

    Add tomcat install script

---
+++ ShellScripts/tomcat.sh
@@ -0,0 +1,34 @@
+useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
+wget http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.27/bin/apache-tomcat-9.0.27.tar.gz -P /tmp
+tar xf /tmp/apache-tomcat-9*.tar.gz -C /opt/tomcat
+ln -s /opt/tomcat/apache-tomcat-9.0.27 /opt/tomcat/latest
+chown -RH tomcat: /opt/tomcat/latest
+sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
+sudo cat << 'EOF' > /etc/systemd/system/tomcat.service
+[Unit]
+Description=Tomcat 9 servlet container
+After=network.target
+
+[Service]
+Type=forking
+
+User=tomcat
+Group=tomcat
+
+Environment="JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom -Djava.awt.headless=true"
+
+Environment="CATALINA_BASE=/opt/tomcat/latest"
+Environment="CATALINA_HOME=/opt/tomcat/latest"
+Environment="CATALINA_PID=/opt/tomcat/latest/temp/tomcat.pid"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+
+ExecStart=/opt/tomcat/latest/bin/startup.sh
+ExecStop=/opt/tomcat/latest/bin/shutdown.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
