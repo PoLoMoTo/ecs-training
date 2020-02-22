e27d8b6f602343335de5470c58d2748229b04bc0
PoLoMoTo <barnwell9@gmail.com>
2020-02-22 00:36:22 -0500

    Fix syntax error

--- ShellScripts/install-menu.sh
+++ ShellScripts/install-menu.sh
@@ -1,10 +1,10 @@
 while ["$1" != ""]; do
     case $1 in
-        apache )            ./apache.sh
-        mysql )             ./mysql.sh
-        java )              ./java.sh
-        tomcat )            ./tomcat.sh
-        all )               ./apache.sh && ./mysql.sh && ./java.sh && ./tomcat.sh
+        apache )            ./apache.sh;;
+        mysql )             ./mysql.sh;;
+        java )              ./java.sh;;
+        tomcat )            ./tomcat.sh;;
+        all )               ./apache.sh && ./mysql.sh && ./java.sh && ./tomcat.sh;;
         * )                 echo "Usage: install-menu.sh [package] -- Available packages: apache, mysql, java, tomcat, all"
     esac
     shift
